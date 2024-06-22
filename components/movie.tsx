48859272690c3668bc6918dfb07d72871cfb407a
davidlee <yvd0301@gmail.com>
2024-06-22 14:19:39 +0900

    prefetch

--- components/movie.tsx
+++ components/movie.tsx
@@ -1,9 +1,8 @@
-"use client"
+"use client";
 
 import Link from "next/link";
-import styles from "../styles/movie.module.css"
+import styles from "../styles/movie.module.css";
 import { useRouter } from "next/navigation";
-
 
 interface IMovieProps {
     title: string;
@@ -11,18 +10,19 @@ interface IMovieProps {
     poster_path: string;
 }
 
-export default function Movie({title, id, poster_path}: IMovieProps){
-
+export default function Movie({ title, id, poster_path }: IMovieProps) {
     const router = useRouter();
 
     const onClick = () => {
         router.push(`/movies/${id}`);
-    }
+    };
 
     return (
         <div className={styles.movie}>
-            <img src={poster_path} alt={title} onClick={onClick}  />
-            <Link href={`/movies/${id}`}>{title}</Link>
+            <img src={poster_path} alt={title} onClick={onClick} />
+            <Link prefetch href={`/movies/${id}`}>
+                {title}
+            </Link>
         </div>
     );
 }
